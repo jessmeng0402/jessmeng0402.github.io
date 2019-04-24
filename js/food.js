60efd79af4c3624d5e39c2d3a425ffef1f4189c5
Jessica Meng <jessica.meng0402@gmail.com>
2019-04-24 01:11:23 -0700

    shud fit the entire box now

--- js/food.js
+++ js/food.js
@@ -2,7 +2,9 @@ function changeToText(elmnt,txt) {
   const curr = elmnt.innerHTML;
   elmnt.style.color = "yellow";
   elmnt.innerHTML = txt;
-  elmnt.style.backgroundImage = "url('/picture/" + curr + ".jpg')"
+  //elmnt.style.backgroundImage = "url(https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwid4t3Ao-jhAhWqIDQIHfUxCa8QjRx6BAgBEAU&url=https%3A%2F%2Fmykoreankitchen.com%2Fkorean-fried-chicken%2F&psig=AOvVaw3hJXf7TEk28Xy8eVmrHIKb&ust=1556179761097739)"
+  elmnt.style.backgroundImage = "url('/picture/" + curr + ".jpg')";
+  elmnt.style.backgroundSize = "cover";
 }
 
 function changeTextBack(elmnt, txt){
